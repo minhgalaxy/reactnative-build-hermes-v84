ccf3d7be912c452a745d1a341353e2bc33e32bf9
minhgalaxy <vvminh198@gmail.com>
2023-06-25 21:56:38 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-node node_modules\react-native\cli.js bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output output\index.android.bundle --assets-dest output\res --sourcemap-output output\sourcemaps\index.android.bundle.packager.map --minify false
+node node_modules\react-native\cli.js bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output output\index.android.bundle.js --assets-dest output\res --sourcemap-output output\sourcemaps\index.android.bundle.packager.map --minify false
 
-node_modules\hermes-engine\win64-bin\hermesc -emit-binary -out output\index.android.bundle.hbc output\index.android.bundle -O -output-source-map
+node_modules\hermes-engine\win64-bin\hermesc -emit-binary -out output\index.android.bundle.hbc output\index.android.bundle.js -O -output-source-map
